137389390a9888b5363f80df98e72d5c3a86a6c3
Alejandro ACER <scann258@hotmail.com>
2019-10-14 16:10:38 -0300

    POST Usuarios

---
+++ server/routes/usuario.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const Usuario = require('../models/usuario');
+
+const app = express();
+
+// GET
+app.get('/usuario', function(req, res) {
+    res.json('get usuario')
+});
+
+
+// POST
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    let usuario = new Usuario({
+
+        nombre: body.nombre,
+        email: body.email,
+        password: body.password,
+        role: body.role
+    });
+
+    usuario.save((err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+
+    });
+
+});
+
+
+// PUT
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    })
+});
+
+
+// DELETE
+app.delete('/usuario', function(req, res) {
+    res.json('delete usuario')
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,60 +1,33 @@
 require('./config/config')
 
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const app = express();
+
+const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
- 
 // parse application/json
 app.use(bodyParser.json());
 
 
+// Rutas
+app.use(require('./routes/usuario'));
 
-// GET
-app.get('/usuario', function (req, res) {
-  res.json('get usuario')
+
+//Conectar DB Mongoose
+mongoose.connect('mongodb://localhost:27017/cafe', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
 });
 
-
-// POST
-app.post('/usuario', function (req, res) {
-
-    let body = req.body;
-
-    if ( body.nombre === undefined ) {
-        res.status(400).json({
-            ok: 'false',
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            body
-        })
-    }
-
-});
-
-
-// PUT
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    })
-});
-
-
-// DELETE
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario')
-});
-
-
- 
+// Server
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando el puerto: ', process.env.PORT );
+    console.log('Escuchando el puerto: ', process.env.PORT);
 });
