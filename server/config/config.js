cff893e3c544a4bdd553434baf96c1563af64f3c
Alejandro ACER <scann258@hotmail.com>
2019-10-17 15:01:38 -0300

    Revert "Variable de entorno MONGO"
    This reverts commit b01cd70093c34e372cd12a08510bccf06bd68ba7.

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URL;
+    urlDB = 'mongodb+srv://scann258:Scann@258@cursonodejs-0i51y.mongodb.net/cafe?retryWrites=true&w=majority';
 }
 
 process.env.URLDB = urlDB;
